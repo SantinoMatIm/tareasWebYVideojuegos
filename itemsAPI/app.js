935d14fcc4c1bab7388c62440330706f8a51401a
SantinoMatIm <santinomi1306@gmail.com>
2025-04-24 14:27:11 -0600

    Second initial commit

---
+++ itemsAPI/app.js
@@ -0,0 +1,13 @@
+"use strict"
+
+import express from "express";
+
+const port = 3000;
+
+const app = express();
+
+app.use(express.json())
+
+app.listen(port, ()=>{
+    console.log(`App listening on port: ${port}`)
+})
