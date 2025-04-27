7c2ba2559b3863b78d19b49daf71d972a327f098
SantinoMatIm <santinomi1306@gmail.com>
2025-04-27 01:55:00 -0600

    Added get-items endpoint

--- itemsAPI/app.js
+++ itemsAPI/app.js
@@ -13,3 +13,25 @@ app.use(express.static('./public'));
 app.listen(port, ()=>{
     console.log(`App listening on port: ${port}`)
 })
+
+import items from './public/js/items.js';
+
+//Function to verify if there are no items
+
+function itemExists(array) {
+    return array.length > 0;
+}
+
+app.get('/items', (req, res)=>{
+    if(itemExists(items)) {
+        res.status(200).json ({
+            message: 'These are the items on the catalog: ',
+            items
+        })
+    }
+    else {
+        res.status(404).json({
+            message: 'There are no items on the catalog'
+        })
+    }
+})
