a5d30c5ab759ddc83144372bb18e551846bf18e0
SantinoMatIm <santinomi1306@gmail.com>
2025-04-27 00:46:54 -0600

    Added post-items method

--- itemsAPI/app.js
+++ itemsAPI/app.js
@@ -11,3 +11,56 @@ app.use(express.json())
 app.listen(port, ()=>{
     console.log(`App listening on port: ${port}`)
 })
+
+let items = [];
+
+//Function to verify if the item already exists
+const itemExists = (newItem) => {
+    return items.some(item => item.id === newItem.id || item.name === newItem.name)
+}
+
+//Endpoint to create one or more items
+app.post('createitem/', (req, res)=>{
+        let newItems = req.body;
+        //Convert to an array if only an object is sent
+        if (!Array.isArray(newItems)) {
+            newItems = [newItems];
+        }
+
+        let added = [];
+        let errors = [];
+
+        newItems.forEach(item => {
+            if (!item.id || !item.name || !item.type || !item.effect) {
+                errors.push({
+                    item,
+                    message: 'There are missing attributes for the item (id, name, type, effect)'
+                })
+            }
+            else if (itemExists(item)) {
+                errors.push({
+                    item,
+                    message: 'The item is already on the server'
+                })
+            }
+            else {
+                items.push(item);
+                added.push(item);
+            }
+        });
+
+
+    //Response status
+
+    if (errors.length > 0) {
+        return res.status(409).json({
+            message: 'These items could not be created',
+            errors
+        })
+    }
+    res.status(201).json({
+        messsage: 'These items were succesfully created',
+        items: added
+    })
+
+})
---
+++ itemsAPI/public/js/items.js
@@ -0,0 +1,8 @@
+const items = [
+    {
+        "id": 1,
+        "name": 'fireball',
+        "type": 'zone',
+        "effect": '+5 zone damage'
+    }
+]
