fed002f2e6ca364b957e54f16d72f6afab5a2e05
SantinoMatIm <santinomi1306@gmail.com>
2025-04-28 13:27:11 -0600

    Added tests.js

---
+++ itemsAPI/public/js/tests.js
@@ -0,0 +1,11 @@
+function getItems() {
+    fetch('http://localhost:3000/items', {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/JSON'
+        }
+    })
+    .then(response => response.json())
+    .then(data => console.log('Items retrieved: ', data))
+    .catch(error => console.error('Error while getting items: ', error));
+}
