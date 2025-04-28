ba0cec8293ac73dfd67871bac8f2f8e8c5f683b0
SantinoMatIm <santinomi1306@gmail.com>
2025-04-28 11:44:28 -0600

    Added endpoint delete-item-by-id

--- itemsAPI/app.js
+++ itemsAPI/app.js
@@ -13,3 +13,27 @@ app.use(express.static('./public'));
 app.listen(port, ()=>{
     console.log(`App listening on port: ${port}`)
 })
+
+import items from './public/js/items.js';
+
+//Function to check if the item exists (same as the one in get-item-by-id)
+const idExists = (id) => {
+    return items.find(item => item.id === parseInt(id));
+}
+
+app.delete('/items/:id', (req, res)=>{
+    const item = idExists(req.params.id);
+
+    if (item) {
+        let newItems = items.filter()
+        res.status(200).json({
+            message: `The object with the id ${req.params.id} was deleted`,
+            
+        })
+    }
+    else {
+        res.status(404).json({
+            message: `The object with the id ${req.params.id} doesnt exist`
+        })
+    }
+})
