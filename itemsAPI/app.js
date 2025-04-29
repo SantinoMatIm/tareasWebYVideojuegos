8da9d23bcdabd575f95aba91cec57ea20d83a1d6
SantinoMatIm <santinomi1306@gmail.com>
2025-04-29 00:29:39 -0600

    Added root file endpoint

--- itemsAPI/app.js
+++ itemsAPI/app.js
@@ -11,6 +11,20 @@ app.use(express.json());
 
 app.use(express.static('./public'));
 
+app.get('/', (req, res) =>{
+    fs.readFile('./public/html/helloServer.html', 'utf8',
+        (err, html) => {
+            if(err){
+                res.status(500).send('There was an error: '
+                + err)
+                return
+            }
+            console.log("Sending page...")
+            res.send(html);
+            console.log("Page sent!")
+        })
+})
+
 app.listen(port, ()=>{
     console.log(`App listening on port: ${port}`)
 })
